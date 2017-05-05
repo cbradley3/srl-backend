3ad9e8670bc1885e35bd8bbe27dd566f63055aa2
theClubhou.se <heythere@theclubhou.se>
2017-05-05 15:51:05 -0400

    roles controller in progress

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
-    //
+    public function index()
+    {
+      $role = Role::all();
+
+      return Response::json($role);
+    }
+
+    public function store(Request $request)
+    {
+      $rules=[
+      'name' => 'required',
+      'email' => 'required',
+      'password' => 'required',
+    ];
+
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+    if($validator->fails())
+    {
+      return Response::json(["error" => "You need to fill out all fields."]);
+    }
+
+    }
 }
