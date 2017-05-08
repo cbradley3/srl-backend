a3775477eb8b23b36823ac766727b00314fd274c
theClubhou.se <heythere@theclubhou.se>
2017-05-08 13:16:51 -0400

    Orders and Products Controllers Updated

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,8 +3,88 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Purifier;
+use Response;
+use App\Order;
 
 class OrdersController extends Controller
 {
-    //
+  public function index()
+  {
+    $order = Order::all();
+
+    return Response::json($order);
+  }
+
+  public function store(Request $request)
+  {
+    $rules = [
+      'name' => 'required',
+      'productId' => 'required',
+      'quantity' => 'required',
+      'totalPrice' => 'required',
+      'comment' => 'required',
+    ];
+
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+    if($validator->fails())
+    {
+      return Response::json(["error" => "You need to fill out all fields."]);
+    }
+
+    $order = new Category;
+    $order->name = $request->input('name');
+    $order->productId = $request->input('productId');
+    $order->quantity = $request->input('quantity');
+    $order->totalPrice = $request->input('totalPrice');
+    $order->comment = $request->input('comment');
+    $order->save();
+
+    return Response::json(["success" => "Success! You did it!"]);
+  }
+
+    public function update($id, Request $request)
+    {
+      $rules = [
+        'name' => 'required',
+        'productId' => 'required',
+        'quantity' => 'required',
+        'totalPrice' => 'required',
+        'comment' => 'required',
+      ];
+
+      $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+      if($validator->fails())
+      {
+        return Response::json(["error" => "You need to fill out all fields."]);
+      }
+
+      $order->name = $request->input('name');
+      $order->productId = $request->input('productId');
+      $order->quantity = $request->input('quantity');
+      $order->totalPrice = $request->input('totalPrice');
+      $order->comment = $request->input('comment');
+      $order->save();
+
+      return Response::json(["success" => "Order Updated!"]);
+    }
+
+    public function show($id)
+      {
+        $order = Order::find($id);
+
+        return Response::json($order);
+      }
+
+    public function delete($id)
+      {
+        $order = Order::find($id);
+
+        $order->delete();
+
+        return Respone::json(['success' => 'Order deleted!']);
+      }
 }
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,102 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Purifier;
+use Response;
+use App\Product;
 
 class ProductsController extends Controller
 {
-    //
+    public function index()
+    {
+      $product = Product::all();
+
+      return Response::json($product);
+    }
+
+    public function store(Request $request)
+    {
+      $rules = [
+      'name' => 'required',
+      'categoryId' => 'required',
+      'availability' => 'required',
+      'price' => 'required',
+      'description' => 'required',
+      'image' => 'required',
+    ];
+
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+    if($validator->fails())
+    {
+      return Response::json(["error" => "You need to fill out all fields."]);
+    }
+
+    $product = new Category;
+    $product->name = $request->input('name');
+    $product->categoryId = $request->input('categoryId');
+    $product->availability = $request->input('availability');
+    $product->price = $request->input('price');
+    $product->description = $request->input('description');
+
+    $image = $request->file('image');
+    $imageName = $image->getClientOriginalName();
+    $image->move("storage/", $imageName);
+    $product->image = $request->root(). "/storage/".$imageName;
+
+    $product->save();
+
+    return Response::json(["success" => "Success! You did it!"]);
+    }
+
+    public function update($id, Request $request)
+    {
+      $rules = [
+      'name' => 'required',
+      'categoryId' => 'required',
+      'availability' => 'required',
+      'price' => 'required',
+      'description' => 'required',
+      'image' => 'required',
+    ];
+
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+    if($validator->fails())
+    {
+      return Response::json(["error" => "You need to fill out all fields."]);
+    }
+
+    $product->name = $request->input('name');
+    $product->categoryId = $request->input('categoryId');
+    $product->availability = $request->input('availability');
+    $product->price = $request->input('price');
+    $product->description = $request->input('description');
+
+    $image = $request->file('image');
+    $imageName = $image->getClientOriginalName();
+    $image->move("storage/", $imageName);
+    $product->image = $request->root(). "/storage/".$imageName;
+
+    $product->save();
+
+    return Response::json(["success" => "Product Updated!"]);
+    }
+
+    public function show($id)
+    {
+      $product = Product::find($id);
+
+      return Response::json($product);
+    }
+
+  public function delete($id)
+    {
+      $product = Product::find($id);
+
+      $product->delete();
+
+      return Respone::json(['success' => 'Product deleted.']);
+    }
 }
